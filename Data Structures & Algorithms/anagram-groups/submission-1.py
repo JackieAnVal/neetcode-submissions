6040bd871680d4751d327ce6cf109810ed7ef38d
JackieAnVal <45046537+JackieAnVal@users.noreply.github.com>
2026-05-07 01:05:52 -0600

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list) # defaultdict(list) crea automáticamente una lista vacía si la llave no existe, evitando KeyErrors
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
+
+        """
+        Summary of Toolssorted() function: 
+        Takes any iterable (like a string) and returns a new sorted list of its elements.
+        .join() method: Combines the list of sorted characters back into a string format.
+        .sort() method: Modifies a list in-place. 
+        You cannot use this directly on a string because strings are immutable; you must convert the string to a list first.
+        """
